78f7f8556123aab76cedcaffd625f7e2e3bcaf3e
Drinnn <pedro_lontro@hotmail.com>
2021-07-30 10:12:20 -0300

    feat: add sync variables hooks

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,9 +1,13 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class NetworkPlayer : NetworkBehaviour {
-    [SyncVar] [SerializeField] private string displayName = "Missing Name";
-    [SyncVar] [SerializeField] private Color displayColor;
+    [SerializeField] private TMP_Text displayNameText;
+    [SerializeField] private Renderer displayColorRenderer;
+
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))] [SerializeField] private string displayName = "Missing Name";
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))] [SerializeField] private Color displayColor;
 
 
     [Server]
@@ -14,5 +18,13 @@ public class NetworkPlayer : NetworkBehaviour {
     [Server]
     public void SetDisplayColor() {
         displayColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName) {
+        displayNameText.SetText(newName);
+    }
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor) {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
     }
 }
