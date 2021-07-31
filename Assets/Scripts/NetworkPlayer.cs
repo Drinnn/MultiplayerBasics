2690429d41396f626aa3d88ee495bbcd9921e692
Drinnn <pedro_lontro@hotmail.com>
2021-07-30 23:39:05 -0300

    feat: add command and clientRpc example

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -9,16 +9,27 @@ public class NetworkPlayer : NetworkBehaviour {
     [SyncVar(hook = nameof(HandleDisplayNameUpdated))] [SerializeField] private string displayName = "Missing Name";
     [SyncVar(hook = nameof(HandleDisplayColorUpdated))] [SerializeField] private Color displayColor;
 
+    #region Server
 
     [Server]
     public void SetDisplayName(string newDisplayName) {
         displayName = newDisplayName;
+        RpcLogNewName(newDisplayName);
     }
 
     [Server]
     public void SetDisplayColor() {
         displayColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName) {
+        SetDisplayName(newDisplayName);
+    }
+
+    #endregion
+
+    #region Client
 
     private void HandleDisplayNameUpdated(string oldName, string newName) {
         displayNameText.SetText(newName);
@@ -27,4 +38,16 @@ public class NetworkPlayer : NetworkBehaviour {
     private void HandleDisplayColorUpdated(Color oldColor, Color newColor) {
         displayColorRenderer.material.SetColor("_BaseColor", newColor);
     }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newDisplayName) {
+        Debug.Log(newDisplayName);
+    }
+
+    [ContextMenu("Set Name")]
+    private void SetName() {
+        CmdSetDisplayName("My New Name");
+    }
+
+    #endregion
 }
